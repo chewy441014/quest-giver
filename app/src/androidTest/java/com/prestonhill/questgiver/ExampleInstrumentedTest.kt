3aa19acbf2d1e19c8fae86b1dac0738578d1ded1
Preston Hill <preston.leon.hill@gmail.com>
2026-08-18 21:11:54 -0500

    setup3

--- app/src/androidTest/java/com/prestonhill/questgiver/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/prestonhill/questgiver/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        Assert.assertEquals("com.example.myapplication", appContext.packageName)
+        Assert.assertEquals("com.prestonhill.questgiver", appContext.packageName)
     }
 }
